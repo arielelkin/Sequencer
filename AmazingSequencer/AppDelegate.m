10cdcccb2fe72341b40bf091e0d0e63ceb7bb753
Ariel Elkin <arielelkin@gmail.com>
2014-03-28 13:30:17 +0000

    Add NSTimer and AEAudioFilePlayer based sequencer

--- AmazingSequencer/AppDelegate.m
+++ AmazingSequencer/AppDelegate.m
@@ -7,15 +7,39 @@
 //
 
 #import "AppDelegate.h"
+#import "AEAudioController.h"
+#import "AEAudioFilePlayer.h"
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    AEAudioController *audioController;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+
+    [self startSequencer];
+
     return YES;
+}
+
+- (void)startSequencer {
+    audioController = [[AEAudioController alloc] initWithAudioDescription:[AEAudioController nonInterleavedFloatStereoAudioDescription]];
+
+    NSError *audioControllerStartError = nil;
+    [audioController start:&audioControllerStartError];
+    if (audioControllerStartError) {
+        NSLog(@"Audio controller start error: %@", audioControllerStartError.localizedDescription);
+    }
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(playSample) userInfo:nil repeats:YES];
+}
+
+- (void)playSample {
+    AEAudioFilePlayer *guitarPlayer = [AEAudioFilePlayer audioFilePlayerWithURL:[[NSBundle mainBundle] URLForResource:@"guitar" withExtension:@"caf"] audioController:audioController error:NULL];
+    [guitarPlayer setRemoveUponFinish:YES];
+    [audioController addChannels:@[guitarPlayer]];
 }
 
 @end
